5fc7298cc96fee3858f06cc49a1b7c023ea929fc
tracer-ics2018 <tracer@njuics.org>
2018-09-19 08:49:15 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  08:49:14 up 18 min,  3 users,  load average: 0.05, 0.08, 0.11 7cd05c87ce1b7bc8cea36785c26e4e0bbdf83a47

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
   int is_batch_mode = init_monitor(argc, argv);
 	
 	/* My add */
-	FILE * fp=fopen("../tools/gen-expr/input","r");
+	FILE * fp=fopen("./tools/gen-expr/input","r");
 	if(fp==NULL) {
 		printf("File error");
 		exit(1);
