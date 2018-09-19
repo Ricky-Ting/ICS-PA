22a30a5874b893ea691ee021d8067251fb681c00
tracer-ics2018 <tracer@njuics.org>
2018-09-19 19:37:45 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:37:45 up 35 min,  3 users,  load average: 0.38, 0.52, 0.53 53df49a985961a55f5bf2d0e13d9c24ae0f23b98

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -10,7 +10,7 @@
 
 
 bool check_parentheses(int p, int q);
-uint32_t eval(int p, int q); 
+long long eval(int p, int q); 
 
 
 
@@ -138,7 +138,7 @@ uint32_t expr(char *e, bool *success) {
   return 0;
 }
 
-uint32_t eval(int p, int q)
+long long eval(int p, int q)
 {
 	if(p>q) {
 		printf("This expr is invalid.\n");
@@ -146,7 +146,7 @@ uint32_t eval(int p, int q)
 		return 0xffffffff;           // Use 0xffffffff to indicate the error.
 	}
 	else if (p==q) 
-		return (uint32_t)(strtol(tokens[p].str,NULL,10));
+		return (long long)(strtol(tokens[p].str,NULL,10));
 	else if (check_parentheses(p,q)) 
 		return eval(p+1,q-1);
 	else {
@@ -188,8 +188,8 @@ uint32_t eval(int p, int q)
 		}
 		*/
 
-		uint32_t val1 = eval(p,pos-1);
-		uint32_t val2 = eval(pos+1,q);	
+		long long val1 = eval(p,pos-1);
+		long long val2 = eval(pos+1,q);	
 	//	if(val1==0xffffffff  || val2==0xffffffff)
 		//		return 0xffffffff;
 		switch(op) {
