6d042c98095b2c2843e32744678d2de141d474b7
tracer-ics2018 <tracer@njuics.org>
2018-09-25 21:07:20 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:07:20 up  6:55,  3 users,  load average: 0.02, 0.05, 0.06 beda868a50985526bcdaf900941237d9f2603fd9

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
   int is_batch_mode = init_monitor(argc, argv);
 
 
-
+/*
 	
 	FILE * fp=fopen("./tools/gen-expr/input","r");
 	if(fp==NULL) {
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]) {
 	}
 	fclose(fp);
 
-
+*/
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
