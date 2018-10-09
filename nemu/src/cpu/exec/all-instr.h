70eb1ac0cd31d9e851873d8b24007efae77db8e9
tracer-ics2018 <tracer@njuics.org>
2018-10-09 14:15:03 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:15:03 up 15 min,  5 users,  load average: 0.33, 0.22, 0.18 fb916c2d3ec1413a2f39c5fbb93f2684b27ca32

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -27,3 +27,4 @@ make_EHelper(leave);
 make_EHelper(adc);
 make_EHelper(or);
 make_EHelper(sar);
+make_EHelper(shl);
--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -71,9 +71,10 @@ make_EHelper(sar) {
 }
 
 make_EHelper(shl) {
-  TODO();
+  //TODO();
   // unnecessary to update CF and OF in NEMU
-
+	rtl_shl(&(id_dest->val),&(id_dest->val),&(id_src->val));
+	rtl_update_ZFSF(&(id_dest->val),id_dest->width);
   print_asm_template2(shl);
 }
 
