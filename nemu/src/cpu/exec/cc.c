f9451321f15ce76f99505209eed9aa072ad321ec
tracer-ics2018 <tracer@njuics.org>
2018-10-09 19:37:42 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:37:42 up  3:13,  4 users,  load average: 0.36, 0.21, 0.18 d72fe65d5e8f9acf2bdfefd2e5a53632fab75956

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -11,9 +11,12 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_L, CC_NL, CC_LE, CC_NLE
   };
 
-	uint32_t myZF;
+	uint32_t myZF,myOF,myCF,mySF;
 	
 	rtl_get_ZF(&myZF);
+	rtl_get_OF(&myOF);
+	rtl_get_CF(&myCF);
+	rtl_get_SF(&mySF);
 	
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
@@ -23,7 +26,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_E: *dest=(myZF?1:0); break;
     case CC_BE:
     case CC_S:
-    case CC_L:
+    case CC_L: *dest=((mySF!=myOF)?1:0); break;
     case CC_LE:
       TODO();
     default: panic("should not reach here");
