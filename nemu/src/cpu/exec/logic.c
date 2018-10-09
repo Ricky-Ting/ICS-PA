0b014157886d9649ec4b96e31ca67bd270d4f64c
tracer-ics2018 <tracer@njuics.org>
2018-10-09 10:35:37 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:35:37 up 25 min,  3 users,  load average: 0.25, 0.23, 0.21 b46dc756fe1b051ab5ac18ab97b9d99e3878cde6

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -6,9 +6,15 @@ make_EHelper(test) {
 	rtl_and(&id_dest->val,&id_dest->val,&id_src->val);
 
 	uint32_t flag=0;
+
+	printf("Eflag:%#x\n",cpu.eflags); //debug
+
+
 	rtl_set_CF(&flag);
 	rtl_set_OF(&flag);
 	rtl_update_ZFSF(&id_dest->val,id_dest->width);
+
+	printf("Eflag:%#x\n",cpu.eflags); //debug
 
   print_asm_template2(test);
 }
