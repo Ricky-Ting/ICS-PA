44074af2e083ad3b06de032178c182aa5158f3f1
tracer-ics2018 <tracer@njuics.org>
2018-10-09 21:39:57 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:39:57 up  5:15,  6 users,  load average: 0.20, 0.19, 0.12 a59825c4b8793c397c6ea6252397957e67df41d6

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -2,6 +2,7 @@
 
 make_EHelper(add) {
   //TODO();
+	/*
 	int32_t a=(int32_t) id_dest->val;
 	int32_t b=(int32_t) id_src->val;
 
@@ -27,9 +28,24 @@ make_EHelper(add) {
 	uint32_t uans=(uint32_t) ans;
 	if(uans<c || uans<d)
 					value=1;
-	rtl_set_CF(&value);
+	rtl_set_CF(&value);*/
 
-//	printf("eflags:%#x\n\n",cpu.eflags);
+	rtl_add(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_setrelop(RELOP_LTU, &t0, &t2, &id_dest->val);
+  rtl_or(&t0, &t3, &t0);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_not(&t0, &t0);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
+
 
   print_asm_template2(add);
 
