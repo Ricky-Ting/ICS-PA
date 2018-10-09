10f4cb8852be85c1a5189034af7ab9197f569bcd
tracer-ics2018 <tracer@njuics.org>
2018-10-09 20:02:04 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:02:04 up  3:37,  4 users,  load average: 0.18, 0.14, 0.15 73218338e14e58d1d8858c2af34f9cb1d04cd04a

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -156,7 +156,8 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  //TODO();
+	*dest= ( ( (int32_t)(*src1)<<( (4-width)*8 )) ) >>( (4-width)*8 );
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
