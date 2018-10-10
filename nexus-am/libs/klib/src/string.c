7dc846d695be75836523e87e6b5645824eabc3bf
tracer-ics2018 <tracer@njuics.org>
2018-10-10 19:57:27 +0800

    >  run 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:57:27 up  5:46,  5 users,  load average: 0.28, 0.27, 0.19 30b8a0e383121ca3791dbb1e523a0b5985341d35

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,38 +3,98 @@
 #ifndef __ISA_NATIVE__
 
 size_t strlen(const char *s) {
-  return 0;
+		size_t length=0;
+		while(*s!='\0') {
+			s++;
+			length++;
+		}
+  return length;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+				char *s = dst;
+		 		while( (*s = *src) !=0 ) {
+					s++;
+					src++;
+				}
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+		 	/*
+				strncpy funcion doesn't promise the final dst is ending with '\0'. When there is still space, it fills dst with '\0';
+			*/
+
+
+			char *s=dst;
+		 	while (n>0 && *src!='\0') {
+						*s=*src;
+						s++;
+						src++;
+						n--;
+			}
+			while(n>0) {
+						*s='\0';
+						n--;
+		 	}
+			return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+			char *s= dst + strlen(dst);
+			strcpy(s,src);
+			return dst;
+  
 }
 
 int strcmp(const char* s1, const char* s2) {
+	while( (*s1)==(*s2)  ) {
+			if( (*s1)=='\0')
+					return 0;
+			s1++;
+			s2++;
+	} 
+	
+	return  ((*(unsigned char *)s1) < (*(unsigned char *)s2)) ? -1 : +1; 
+
+
+
   return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+				while(n>0) { 
+						if(*s1!=*s2)
+											return  ((*(unsigned char *)s1) < (*(unsigned char *)s2)) ? -1 : +1; 
+						else if( *s1=='\0')
+										return 0;
+						n--;
+						s1++;   s2++;
+				}
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+				const unsigned char uc=c;
+				unsigned char *dst=v;
+				for(;n>0;n--,dst++)
+								*dst=uc;
+				return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+				unsigned char *o=out;
+				const unsigned char *i=in;
+				for(;n>0;n--,o++,i++) 
+							*o=*i;
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+
+				for(;n>0;n--,s1++,s2++) 
+						if(*(unsigned char *)s1!=*(unsigned char *)s2)
+											return  ((*(unsigned char *)s1) < (*(unsigned char *)s2)) ? -1 : +1; 
   return 0;
 }
 
