fbafd34305133aef5a3c712191ea79e1b8a4a60b
tracer-ics2018 <tracer@njuics.org>
2018-10-12 01:15:52 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  01:15:52 up  7:00,  5 users,  load average: 0.24, 0.46, 0.32 8560968ef2dcde17cad781d96587be453e135a09

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -172,7 +172,7 @@ opcode_entry opcode_table [512] = {
   /* 0x78 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x7c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x80 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x84 */	IDEX(J,jcc), IDEX(J,jcc), EMPTY, EMPTY,
+  /* 0x84 */	IDEX(J,jcc), IDEX(J,jcc), EMPTY, IDEX(J,jcc),
   /* 0x88 */	IDEX(J,jcc), EMPTY, EMPTY, EMPTY,
   /* 0x8c */	EMPTY, EMPTY, IDEX(J,jcc), EMPTY,
   /* 0x90 */	EMPTY, EMPTY, EMPTY, EMPTY,
