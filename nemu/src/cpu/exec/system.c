8da7c92696b23f2da28dbbf6556dc9f3fff64b68
tracer-ics2018 <tracer@njuics.org>
2018-11-12 21:50:11 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:50:11 up  1:46,  5 users,  load average: 0.18, 0.12, 0.10 cc9cda192a5bce53bcda8a0c82f93035a7b8f8dd

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -9,6 +9,7 @@ void difftest_skip_dut();
 make_EHelper(lidt) {
   //TODO();
 	printf("%u\n",id_dest->addr);
+	printf("%u\n",vaddr_read(id_dest->addr,4));
 	rtl_lm(&t1,&id_dest->addr,2);
 	t0=id_dest->val+2;
 	rtl_lm(&t2,&t0,4);
