0990fd3c7e9d630edd7e6cfef32c6b4fa10f29b6
tracer-ics2018 <tracer@njuics.org>
2018-11-14 23:53:48 +0800

    >  run 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  23:53:48 up  3:04,  8 users,  load average: 0.16, 0.15, 0.22 fa5408cc41cbada71e8c8191327ed4db65a2923

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -26,7 +26,7 @@ int main() {
   init_fs();
 
   init_proc();
-
+printf("hi\n");
 #ifdef HAS_CTE
   _yield();
 #endif
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,6 +6,7 @@ _Context* do_syscall(_Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
