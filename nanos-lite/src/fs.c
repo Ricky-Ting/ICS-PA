127ac4b674021a00f529249e7c90d402101c0192
tracer-ics2018 <tracer@njuics.org>
2018-11-18 14:39:44 +0800

    >  run 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:39:44 up  5:15,  4 users,  load average: 1.72, 1.18, 0.90 523a0d4b12181b0a3f901732beea5f7b85610dd4

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -41,7 +41,7 @@ static Finfo file_table[] __attribute__((used)) = {
 
 int fs_open(const char *pathname, int flags, int mode) {
 	printf("fs_open: %s\n",pathname);
-	for(int i=3;i<NR_FILES;i++)
+	for(int i=0;i<NR_FILES;i++)
 				 	if(strcmp(pathname,file_table[i].name)==0) {
 						file_table[i].open_offset=0;
 						return i;
