906285a0d7d8d3af72fe8f221b55b918554fe781
tracer-ics2018 <tracer@njuics.org>
2018-11-18 14:18:58 +0800

    >  run 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:18:58 up  4:55,  3 users,  load average: 0.18, 0.28, 0.46 277747e9899dc25dc00cb023e12fbef468bda924

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -42,7 +42,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 	tmp[len-1]='\0';
 	len=strlen(tmp);
 	//printf("len=%d\n",len);
-	memcpy(buf,(void *)tmp,len+1);
+	memcpy(buf,(void *)tmp,len+2);
 	char newhead[200];
 	memcpy(newhead,buf,len+1);
 	//printf("tmp =  %s",newhead);
