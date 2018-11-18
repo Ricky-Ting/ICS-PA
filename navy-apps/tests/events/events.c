a7d48dbbc6b5dc90825647c67f4c3867d73f4e6b
tracer-ics2018 <tracer@njuics.org>
2018-11-18 14:55:41 +0800

    >  run 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:55:41 up  5:31,  4 users,  load average: 1.23, 0.98, 0.85 3cdbc558facc2c5d51772a53b4fcb8c253254b11

--- navy-apps/tests/events/events.c
+++ navy-apps/tests/events/events.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
 int main() {
-	printf("hihi\n");
   FILE *fp = fopen("/dev/events", "r");
-	printf("hi\n");
   volatile int j = 0;
   while(1) {
     j ++;
     if (j == 1000000) {
-			printf("l\n");
       char buf[256];
       char *p = buf, ch;
       while ((ch = fgetc(fp)) != -1) {
-				printf("In p=%s\n",ch);
         *p ++ = ch;
         if(ch == '\n') {
           *p = '\0';
