796dcf612ea6f3cc5dfc3ce28b082b61dc098461
tracer-ics2018 <tracer@njuics.org>
2018-11-18 12:44:41 +0800

    >  run 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  12:44:41 up  3:20,  6 users,  load average: 0.06, 0.16, 0.17 9adfc0843f74b05758f9e250f1bd4428c219ad32

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -39,6 +39,8 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   int x,y;
 	uint32_t pixel;
+	
+
 	for(size_t i=0;i<len;i+=4) {
 			memcpy(&pixel,buf+i,4);
 			y=offset/screen_width();
