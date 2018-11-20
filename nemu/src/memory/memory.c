f4d6739a44e3ff8e1fbcbd2af2021702c18b6f6d
tracer-ics2018 <tracer@njuics.org>
2018-11-21 01:47:34 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  01:47:34 up  2:39,  1 user,  load average: 0.83, 0.78, 0.84 e7dbf33d63bec92b74c8082ab134cf45fea089aa

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+
 #include "device/mmio.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
@@ -13,17 +14,17 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-	if(is_mmio(addr)==-1) 
-  	return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
-	else
-		return mmio_read(addr,len,is_mmio(addr));
+  int post = is_mmio(addr);
+  if(post != -1)
+    return mmio_read(addr, len, post);
+  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, uint32_t data, int len) {
-	if(is_mmio(addr)==-1)
-  	memcpy(guest_to_host(addr), &data, len);
-	else
-		mmio_write(addr,len,data,is_mmio(addr));
+  int post = is_mmio(addr);
+  if(post != -1)
+    mmio_write(addr, len, data, post);
+  memcpy(guest_to_host(addr), &data, len);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
