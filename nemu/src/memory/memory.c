f8f072e3edfe65fac42bb7737e1320e185e3c63c
tracer-ics2018 <tracer@njuics.org>
2018-12-18 22:52:08 +0800

    >  compile 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:52:08 up  3:26,  5 users,  load average: 0.37, 0.26, 0.15 d2c84488bdcf41146d12529b2f8e0cf7b0654819

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -9,7 +9,7 @@
     })
 
 uint8_t pmem[PMEM_SIZE];
-
+paddr_t page_translate(vaddr_t addr);
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
@@ -27,9 +27,35 @@ void paddr_write(paddr_t addr, uint32_t data, int len) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+	if ( ((addr+len-1)&0xfff) < ((addr)&0xfff)  )			
+					assert(0);
+	else {
+					paddr_t paddr = page_translate(addr);
+  				return paddr_read(paddr, len);
+	}
 }
 
 void vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  paddr_write(addr, data, len);
+ 	if ( ((addr+len-1)&0xfff) < ((addr)&0xfff)  )			
+					assert(0);
+	else {
+	 				paddr_t paddr = page_translate(addr);	
+					paddr_write(paddr, data, len);
+	}
 }
+
+paddr_t page_translate(vaddr_t addr) {
+					uint32_t pagedir = addr>>22;
+					uint32_t page = (addr>>12)&0x3ff;
+					uint32_t offset = (addr)&0xfff;
+
+					uint32_t dir_entry=paddr_read(cpu.CR3+(pagedir)*4, 4 );
+					assert(dir_entry&0x1);
+					uint32_t table_entry = paddr_read( ((dir_entry)&0xfff) + (page)*4, 4  );
+					assert(table_entry&0x1);
+					paddr_t ret = (table_entry&0xfff) + offset;
+					return ret;
+
+				
+}
+
