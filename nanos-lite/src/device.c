4ac487f90a9d868b06a95abf4a442adeda952083
tracer-ics2018 <tracer@njuics.org>
2018-12-20 01:20:59 +0800

    >  run 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  01:20:58 up  8:53,  4 users,  load average: 0.40, 1.45, 1.65 1551deb86a3cb2d1dc14f5dac7254447d2fd46bc

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -31,24 +31,24 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 		if(key==_KEY_F1 ) {
 			if(down) {
 					fg_pcb=1;
-					printf("Change to 1\n");
+					Log("Change to 1\n");
 					_yield();
 			}
 		}
 		else if(key==_KEY_F2) {
 						if(down) {
 								fg_pcb=2;
-								printf("Change to 2\n");
+								Log("Change to 2\n");
 								_yield();
-						}
+						} 
 		} 
 		else if(key==_KEY_F3) {
 						if(down) {
 								fg_pcb=3;
-								printf("Change to 3\n");
+								Log("Change to 3\n");
 								_yield();
-						}
-		}
+						} 
+		} 
 		else {
 			if(down) {
 				sprintf(buf,"kd %s\n",keyname[key]);				
