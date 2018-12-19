8283a56eb4ab19d6b43a5847cfed338902d7ea7b
tracer-ics2018 <tracer@njuics.org>
2018-12-20 00:15:17 +0800

    >  run 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:15:17 up  7:47,  4 users,  load average: 0.10, 0.23, 0.40 267ecf81b01a281e8ab8dadcbcdfb7fed12cb830

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -7,6 +7,7 @@ static _Context* (*user_handler)(_Event, _Context*) = NULL;
 void vectrap();
 void vecsys();
 void vecnull();
+void irq0();
 void get_cur_as(_Context *c);
 void _switch(_Context *c);
 
@@ -21,7 +22,7 @@ _Context* irq_handle(_Context *tf) {
 			case 0x80 : ev.event=_EVENT_SYSCALL;  break;
 			case 0x81 : ev.event=_EVENT_YIELD; break;
 			case 32 : ev.event=_EVENT_IRQ_TIMER; break;
-      default: printf("In irq_handle %d\n",tf->irq); ev.event = _EVENT_ERROR; break;
+      default:  ev.event = _EVENT_ERROR; break;
     }
 
     next = user_handler(ev, tf);
@@ -45,6 +46,7 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
   // -------------------- system call --------------------------
   idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_KERN);
 	idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vecsys , DPL_KERN);
+	idt[32] = GATE(STS_TG32,KSEL(SEG_KCODE),irq0,DPL_KERN);
   set_idt(idt, sizeof(idt));
 
   // register event handler
