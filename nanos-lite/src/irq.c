253556e22c501b75bbcc0ceebeebe6ab09840aed
tracer-ics2018 <tracer@njuics.org>
2018-12-20 01:19:42 +0800

    >  run 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  01:19:42 up  8:52,  4 users,  load average: 1.21, 1.84, 1.78 d680f617d45696103cc92733556b5ef72122176d

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,7 @@ static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
 		case _EVENT_SYSCALL:  do_syscall(c); break;
 		case _EVENT_YIELD :  return schedule(c);    break;
-		case _EVENT_IRQ_TIMER: Log("This is _EVENT_IRQ_TIMER\n"); _yield(); break;
+		case _EVENT_IRQ_TIMER:/* Log("This is _EVENT_IRQ_TIMER\n");*/ _yield(); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
