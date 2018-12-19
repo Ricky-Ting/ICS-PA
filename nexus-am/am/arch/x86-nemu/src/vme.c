51697482bfead7a32b492b128aefc8d019e9e038
tracer-ics2018 <tracer@njuics.org>
2018-12-19 15:42:38 +0800

    >  run 171860509 ricky Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:42:38 up  1:45,  5 users,  load average: 0.09, 0.14, 0.16 d21a292a9c8c278a3dbd150638ae13439a8ab233

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -42,7 +42,7 @@ int _vme_init(void* (*pgalloc_f)(size_t), void (*pgfree_f)(void*)) {
       }
     }
   }
-
+	printf("In vme_init here here\n");
   set_cr3(kpdirs);
   set_cr0(get_cr0() | CR0_PG);
 	printf("sre cr0\n");
